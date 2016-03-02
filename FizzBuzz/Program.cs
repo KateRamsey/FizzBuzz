1b8fce62bbdb64dd8a5e726af3b822247004288e
Kate Ramsey <kateramsey@live.com>
2016-03-02 16:58:56 -0600

    add logic

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,27 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            bool FizzOrBuzz = false;
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i%3 == 0)
+                {
+                    Console.Write("Fizz");
+                    FizzOrBuzz = true;
+                }
+                if(i%5 == 0)
+                {
+                    Console.Write("Buzz");
+                    FizzOrBuzz = true;
+                }
+                if(!FizzOrBuzz)
+                {
+                    Console.Write(i);
+                }
+                FizzOrBuzz = false;
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
